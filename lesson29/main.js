caf9186d5760f4dea3cc7a0b2b0d5d2a52b3c166
Hentai277353 <91971896+Hentai277353@users.noreply.github.com>
2022-01-15 19:56:15 +0200

    Add files via upload

---
+++ lesson29/main.js
@@ -0,0 +1,253 @@
+//let userName = 'ggghvui'
+//let seconduserName = 'olya'
+//let age = prompt('ведите возраст')
+
+//alert(`я ${userName} имя ${seconduserName} мне ${age}`)
+
+
+// let a = prompt('a = ')
+// let p = 4*a
+
+// alert(`p = ${p}`)
+
+//let a = prompt('a = ')
+//let S = a*4
+
+//alert(`S = ${S}`)
+
+//let a = prompt('a =')
+//let b = prompt('b = ')
+//let p = 2*(a*1+b*1)
+//let S = a*1*b*1
+
+//alert(`p = ${p} S = ${S} `)
+
+//let d = prompt('d = ')
+//let p = 3.14
+//let l = p*d
+
+//alert(`l = ${l} p = ${p} d = ${d}`)
+
+//let a = prompt('a = ')
+//let V = a^3
+//let S = 6*a
+
+//alert(`a = ${a} V = ${V} S = ${S}`)
+
+// let a = prompt('a =')
+// let b = prompt('b = ')
+// let c = prompt('c = ')
+// let V = a*b*c
+// let S = 2*(a*b+b*c+a*c)
+
+// alert(`a = ${a} b = ${b} c = ${c} V = ${V} S = ${S}`)
+
+// let p = 3.14
+// let R = ('R = ')
+// let L = 2*p*R
+// let S = p*R*R
+
+// alert(`L = ${L} p = ${p} R = ${R} S = ${S}`)
+
+// let a = prompt('a =')
+// let b = prompt('b = ')
+// let p = (a+b)/2
+
+// alert(`a = ${a} b = ${b} p = ${p}`)
+
+// let a = prompt('a =')
+// let b = prompt('b = ')
+// let sqrt = a*b
+
+// alert(`a = ${a} b = ${b} sqrt = ${sqrt}`)
+
+// let a = prompt('a =')
+// let b = prompt('b = ')
+// let p = a*1+b*1
+// let g = a*b
+// let o = a-b
+// let l = a/b
+
+// alert(`p = ${p} a = ${a} b = ${b} g = ${g} o = ${o} l = ${l}`)
+
+// let a = prompt('a =')
+// let b = prompt('b = ')
+// let p = a*1+b*1
+// let g = a*b
+// let o = a-b
+
+
+// alert(`p = ${p} a = ${a} b = ${b} g = ${g} o = ${o} `)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let c = a*a+b*b
+// let p = a+b+c
+
+// alert(`a = ${a} b = ${b} c = ${c} p = ${p}`)
+
+// let S = ('S =')
+// let D = L/p
+// let p = 3.14
+// let L = p*D
+// let k = p*D*D/4
+
+// alert(`S = ${S} D = ${D} p = ${p} L = ${L} k = ${k}`)
+
+
+// let L = prompt('L = ')
+// let L = 2*p*R
+// let S = p*R
+
+// alert(`L = ${L} S = ${S}`)
+
+// let L = prompt('L = ')
+// let L = L/100
+
+// alert(`L = ${L}`)
+
+// let M = prompt('M = ')
+// let a = 1000
+// let b = M/a
+
+// alert(`M = ${M} a = ${a} b = ${b}`)
+
+// let a = prompt('a = ')
+// let b = 1025
+// let g = a/b
+
+// alert(`a = ${a} b = ${b} g = ${g}`)
+
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let k = a/b
+
+// alert(`a = ${a} b = ${b} k = ${k}`)
+
+// let a = prompt('a = ')
+// let b = prompt('b = ')
+// let k = b/a
+
+// alert(`a = ${a} b = ${b} k = ${k}`)
+
+// let a = 10
+// let b = 1
+// let g = 10/2
+// let h = g - 1
+// let x = ('x = ')
+
+// alert(`a = ${a} b = ${b} g = ${g} h = ${h}`)
+
+// let a = 65
+// let b = 6
+// let c = 5
+// let g = 11
+
+// alert(`a = ${a} b = ${b} c = ${c} g = ${g}`)
+
+// let a = 65
+// let b = 6
+// let c = 5
+// let g = 56
+
+// alert(`a = ${a} b = ${b} c = ${c} g = ${g}`)
+
+let a = 356
+let b = 356/100
+let c = 178
+let d = 100
+
+alert(`a = ${a} c = ${c} b = ${b} d = ${d}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
